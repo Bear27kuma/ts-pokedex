ceca420af087939b9d3eca74d9920dd3fe599e9c
Yuta Okuma <y.dream.nest@icloud.com>
2022-05-17 01:42:15 +0900

    APIのレスポンスで使用するオブジェクトの型定義

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
+// Pokedexクラスをインポートする
+import Pokedex from './pokemon';
+
 // HTML要素を取得する
 const container: HTMLElement | null = document.getElementById('app');
+const pokemons = 1;
+
+const pokedex = new Pokedex(pokemons);
+pokedex.fetchData();
--- src/pokemon.ts
+++ src/pokemon.ts
@@ -1,10 +1,84 @@
-// 画面表示で使用するpokemonオブジェクトの型定義
+// PokeAPIのベースURL
+const baseUrl = 'https://pokeapi.co/api/v2/pokemon/';
+
+// APIのレスポンスで使用するオブジェクトの型定義
 interface Pokemon {
   id: number;
   name: string;
-  image: string;
+  sprites: {
+    front_default: string;
+  };
   type: string;
+  species: {
+    url: string;
+  };
+  types: Array<{
+    type: {
+      url: string;
+    };
+  }>;
 }
 
 // 非同期処理の実行完了後の値を定義する
-type FetchPokemon = (id: number) => Promise<void>;
+type FetchPokemon = (id: number) => Promise<void | null>;
+
+export default class Pokedex {
+  // pokemonsプロパティを定義
+  pokemons: number;
+
+  // constructorで初期化する
+  constructor(pokemons: number) {
+    this.pokemons = pokemons;
+  }
+
+  fetchData(): void {
+    for (let i = 1; i <= this.pokemons; i++) {
+      void this.getPokemon(i);
+    }
+  }
+
+  private getPokemon: FetchPokemon = async (id: number) => {
+    const response: Response | null = await fetch(`${baseUrl}${id}`)
+      .then((res) => res)
+      .catch((error) => {
+        console.error(error);
+        return null;
+      });
+
+    if (!response) {
+      return null;
+    }
+
+    const json = await response
+      .json()
+      .then((json: Pokemon) => {
+        return json;
+      })
+      .catch((error) => {
+        console.error(error);
+        return null;
+      });
+
+    if (!json) {
+      return null;
+    }
+
+    console.log(json);
+
+    const transformedPokemon = {
+      id: json.id,
+      name: json.name,
+      image: json.sprites.front_default,
+      species: json.species.url,
+      types: json.types,
+    };
+
+    console.log(transformedPokemon);
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    // const pokemon = await data.json();
+    // const pokemonType: string = pokemon.types
+    //   .map((poke: any) => poke.type.name)
+    //   .join(', ');
+  };
+}
