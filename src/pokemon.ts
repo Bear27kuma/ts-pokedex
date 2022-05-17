d6a900c566972e1ae7ec3b0a326b47ff4d063ff8
Yuta Okuma <y.dream.nest@icloud.com>
2022-05-17 14:07:24 +0900

    タイプの日本語名を取得する

--- src/pokemon.ts
+++ src/pokemon.ts
@@ -1,9 +1,7 @@
 // PokeAPIのベースURL
-import * as url from 'url';
-
 const baseUrl = 'https://pokeapi.co/api/v2/pokemon/';
 
-// APIのレスポンスで使用するオブジェクトの型定義
+// PokeAPIで使用するpokemonオブジェクトの型定義
 interface Pokemon {
   id: number;
   name: string;
@@ -14,16 +12,26 @@ interface Pokemon {
   species: {
     url: string;
   };
-  types: Array<{
-    type: {
-      name: string;
-      url: string;
-    };
-  }>;
+  types: { type: { name: string; url: string } }[];
+}
+
+// タイプ情報の型定義
+interface Types {
+  names: { name: string }[];
+}
+
+// レスポンスを整形したオブジェクトの型定義
+interface ResponsePokemon {
+  id: number;
+  name: string;
+  image: string;
+  species: string;
+  types: { type: { name: string; url: string } }[];
 }
 
 // 非同期処理の実行完了後の値を型定義する
 type FetchPokemon = (id: number) => Promise<void | null>;
+type FetchType = (typesUrl: string[]) => Promise<string[] | null>;
 
 export default class Pokedex {
   // pokemonsプロパティを定義
@@ -52,9 +60,10 @@ export default class Pokedex {
       return null;
     }
 
-    const json = await response
+    const json: Pokemon | null = await response
       .json()
       .then((json: Pokemon) => {
+        console.log(json);
         return json;
       })
       .catch((error) => {
@@ -66,9 +75,8 @@ export default class Pokedex {
       return null;
     }
 
-    console.log(json);
-
-    const responsePokemon = {
+    // レスポンスを整形する
+    const responsePokemon: ResponsePokemon = {
       id: json.id,
       name: json.name,
       image: json.sprites.front_default,
@@ -76,10 +84,10 @@ export default class Pokedex {
       types: json.types,
     };
 
-    console.log(responsePokemon);
-
+    // タイプのURLを格納する配列
     const typesUrl: string[] = [];
 
+    // レスポンスからタイプの情報を抜き出す
     responsePokemon.types.forEach((types: { type: { url: string } }) => {
       Object.keys(types).forEach((category: string) => {
         if (category === 'type') {
@@ -88,11 +96,51 @@ export default class Pokedex {
       });
     });
 
-    console.log(typesUrl);
+    // タイプの日本語名を取得する
+    const types: string[] | null = await this.getJpTypes(typesUrl);
+    console.log(types);
 
     // const pokemon = await data.json();
     // const pokemonType: string = pokemon.types
     //   .map((poke: any) => poke.type.name)
     //   .join(', ');
+  };
+
+  // タイプの日本語を取得する
+  private getJpTypes: FetchType = async (typesUrl: string[]) => {
+    const jpTypes: string[] = [];
+
+    // ループで各URLをfetchして日本語名を配列にpushする
+    for (const url of typesUrl) {
+      const response: Response | null = await fetch(url)
+        .then((res) => res)
+        .catch((error) => {
+          console.error(error);
+          return null;
+        });
+
+      if (!response) {
+        return null;
+      }
+
+      const json: Types | null = await response
+        .json()
+        .then((json: Types) => {
+          console.log(json);
+          return json;
+        })
+        .catch((error) => {
+          console.log(error);
+          return null;
+        });
+
+      if (!json) {
+        return null;
+      }
+
+      jpTypes.push(json.names[0].name);
+    }
+
+    return jpTypes;
   };
 }
