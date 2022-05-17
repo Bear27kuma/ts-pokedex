aa6262077f7d38d1345b7ff7fd2de978f92b4991
Yuta Okuma <y.dream.nest@icloud.com>
2022-05-17 18:17:32 +0900

    null判定の関数をindex.tsに移動させる

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 // Pokedexクラスをインポートする
 import Pokedex from './pokemon';
 
+// 組み込みユーティリティとジェネリクスを使ってnull判定を行う
+export const isNonNullable = <T>(value: T): value is NonNullable<T> => {
+  return !(value === null || value === void 0);
+};
+
 // HTML要素を取得する
 const container: HTMLElement | null = document.getElementById('app');
 const pokemons = 5;
 
-const pokedex = new Pokedex(pokemons);
-void pokedex.fetchData();
+if (isNonNullable(container)) {
+  const pokedex = new Pokedex(pokemons, container);
+  void pokedex.fetchData();
+} else {
+  console.error('There is some error in getting the HTMLElement.');
+}
