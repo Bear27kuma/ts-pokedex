cb1c5c541b626d7764c2bf7bad1a622ccee04642
Yuta Okuma <y.dream.nest@icloud.com>
2022-05-19 20:39:32 +0900

    HTMLのボタンから関数を実行できるよう修正

--- src/index.ts
+++ src/index.ts
@@ -9,13 +9,17 @@ export const isNonNullable = <T>(value: T): value is NonNullable<T> => {
   return !(value === null || value === void 0);
 };
 
-// HTML要素を取得する
+// 生成したHTMLを挿入するdiv要素を取得する
 const container: HTMLElement | null = document.getElementById('app');
-const pokemons = 9;
+let pokedex: Pokedex;
 
+// document.getElementById()で取得したHTML要素のnull判定を行う
 if (isNonNullable(container)) {
-  const pokedex = new Pokedex(pokemons, container);
-  void pokedex.fetchData();
+  pokedex = new Pokedex(container);
 } else {
   console.error('There is some error in getting the HTMLElement.');
 }
+
+const exec = async () => {
+  await pokedex.fetchData(1, 151);
+};
